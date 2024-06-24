34d1ed46fb5cebec10d452625b2edd926585bc1a
KafuiMawunyo <siabikafui123@gmail.com>
2024-06-24 04:40:01 -0700

    adding all files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// alert('Hi my name is Siabi Mawunyo Kafui')
+// let name1 = prompt('What is your name?');
+//  alert(name1);
+
+//  alert('I am 30 years old')
+//  let age = prompt('what is your age')
+//  alert(age)
+
+//  alert('I come from the Volta Region')
+let name1 = prompt('what is your name?')
+ alert(name1);
+ alert('hi ' + name1 + ' ,Welcome to our website');
+ 
