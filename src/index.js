562e943d2e944e9090038759b4a7930fdedb8dfe
KafuiMawunyo <siabikafui123@gmail.com>
2024-06-24 05:15:33 -0700

    adding all files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@
 //  alert(age)
 
 //  alert('I come from the Volta Region')
-let name1 = prompt('what is your name?')
- alert(name1);
- alert('hi ' + name1 + ' ,Welcome to our website');
- 
+// let age = 18;
+// let hometown = 'Accra';
+let name1 = prompt('what is your name?');
+let title = prompt('what is your title?');
+let favoriteproduct = prompt('what is your favorite product');
+//  alert(name1);
+//  alert('hi ' + name1 + ' ,Welcome to our website');
+ alert(`Hi ${title},  ${name1}, do you like ${favoriteproduct}`);
